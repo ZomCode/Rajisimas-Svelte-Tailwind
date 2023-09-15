452537435a526ef04b5e9e3c8d06974187c4bc0a
ZomDev <kevinyt.kg@gmail.com>
2023-09-15 01:07:32 -0600

    Ajustando idiomas v3

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,5 @@
 <script>
 	import '../app.css';
-<<<<<<< HEAD
-=======
-	import { _, setupI18n, isLocaleLoaded } from '../services/i18n';
->>>>>>> 89779193b19175cfbac3629b77801e2603441d80
 
 	import Home from '../views/Home.svelte';
 	import WhereShop from '../views/Where.svelte';
@@ -75,57 +71,9 @@
 			</div>
 		</div>
 	{/if}
-<<<<<<< HEAD
 	
 	<Router>
 		<Header/>
-=======
-
-	<Router>
-		<nav class="flex justify-between md:justify-around items-center bg-yellow-400 p-8 text-2xl">
-			<div class="flex items-center">
-				<Link to="/">
-					<img src={imgLogo} alt="LogoRajisimas" class="h-auto w-20" />
-				</Link>
-				<Link to="/">
-					<img src={LetrasR} alt="LogoRajisimas" class="h-auto w-20" />
-				</Link>
-			</div>
-
-			<!-- Menú desplegable para pantallas pequeñas -->
-			<div class="md:hidden">
-				<button class="text-white" on:click={toggleMenu}>
-					<svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
-							d={isMenuOpen ? 'M6 18L18 6M6 6l12 12' : 'M4 6h16M4 12h16M4 18h16'}
-						/>
-					</svg>
-				</button>
-			</div>
-
-			<!-- Menú principal para pantallas medianas y grandes -->
-			<div class="hidden md:flex space-x-4 font-bold">
-				<Link to="/" class="nav-link hover:text-white">{$_('header.home')}</Link>
-				<Link to="/Where" class="nav-link hover:text-white">{$_('header.whereshop')}</Link>
-				<Link to="/We" class=" hover:text-white">{$_('header.about')}</Link>
-			</div>
-		</nav>
-
-		<!-- Menú desplegable con efecto de transición -->
-		{#if isMenuOpen}
-			<div class="md:hidden transition-transform duration-500 ease-in-out font-bold text-1xl">
-				<div class="flex flex-col space-y-4 p-4 bg-yellow-400">
-					<Link to="/" class="nav-link">{$_('header.home')}</Link>
-					<Link to="/Where" class="nav-link">{$_('header.whereshop')}</Link>
-					<Link to="/We" class="nav-link">{$_('header.about')}</Link>
-				</div>
-			</div>
-		{/if}
-
->>>>>>> 89779193b19175cfbac3629b77801e2603441d80
 		<Route path="/">
 			<Home />
 		</Route>
@@ -135,11 +83,7 @@
 		<Route path="/We">
 			<We />
 		</Route>
-<<<<<<< HEAD
 		<Route component={NotFound}></Route>
-=======
-		<Route path="*" component={NotFound} />
->>>>>>> 89779193b19175cfbac3629b77801e2603441d80
 	</Router>
 	<Footer />
 {:else}
