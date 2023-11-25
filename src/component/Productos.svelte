d2edefdaaea9fa48928b55c5fb1c6f28e7a6f730
ZomDev <kevinyt.kg@gmail.com>
2023-11-24 22:27:09 -0600

    Otra forma para el fondo v2

--- src/component/Productos.svelte
+++ src/component/Productos.svelte
@@ -71,7 +71,7 @@
 		[&>div]:animate-fade-up animate-once animate-duration-1000 animate-ease-in
 		first:bg-red-900
 		"
-	style={[`background-image: url(${FondoDesktop})`]}
+	style={[`background-image: url(../img/FondoDesktop.webp)`]}
 	>
 		<h2 class="font-bold text-4xl sm:text-5xl p-6 uppercase w-full text-center">
 			{$_('home.title')}
